ffb949119a9ba9f30bf3793ddc79107c22b353ca
ilhamfikri08 <60727213+ilhamfikri08@users.noreply.github.com>
2021-02-23 14:12:49 +0700

    Update View Galeri index.php

--- application/views/galeri/index.php
+++ application/views/galeri/index.php
@@ -6,56 +6,16 @@
             </div>
         </div>
         <div class="row galerys">
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar2.png') ?>" target="_blank" title="tes"> <img src="<?= base_url('assets/img/gambar2.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
+            <?php
+            foreach ($row as $data) {
+            ?>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
+                    <a href="<?= base_url('assets/galeri/' . $data->image_galeri) ?>" target="_blank" title="<?= $data->judul ?>"> <img src="<?= base_url('assets/galeri/' . $data->image_galeri) ?>" alt="gambar cis" class="img-fluid">
+                    </a>
+                </div>
+            <?php } ?>
 
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/12.png') ?>" target="_blank" title="tes2"> <img src="<?= base_url('assets/img/12.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar3.png') ?>" target="_blank" title="tes3"> <img src="<?= base_url('assets/img/gambar3.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar4.png') ?>" target="_blank" title="tes4"> <img src="<?= base_url('assets/img/gambar4.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar1.png') ?>" target="_blank" title="tes5"> <img src="<?= base_url('assets/img/gambar1.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar3.png') ?>" target="_blank" title="tes6"> <img src="<?= base_url('assets/img/gambar3.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar4.png') ?>" target="_blank" title="tes7"> <img src="<?= base_url('assets/img/gambar4.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
 
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar4.png') ?>" target="_blank" title="tes7"> <img src="<?= base_url('assets/img/gambar4.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar2.png') ?>" target="_blank" title="tes7"> <img src="<?= base_url('assets/img/gambar2.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar1.png') ?>" target="_blank" title="tes7"> <img src="<?= base_url('assets/img/gambar1.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar4.png') ?>" target="_blank"> <img src="<?= base_url('assets/img/gambar4.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-6 col-xl-3  pt-3">
-                <a href="<?= base_url('assets/img/gambar2.png') ?>" target="_blank"> <img src="<?= base_url('assets/img/gambar2.png') ?>" alt="gambar cis" class="img-fluid">
-                </a>
-            </div>
         </div>
 
     </div>
