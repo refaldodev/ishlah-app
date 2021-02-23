ddecdbf6a27e6c834879838038f3c03e2985794c
ilhamfikri08 <60727213+ilhamfikri08@users.noreply.github.com>
2021-02-23 14:11:52 +0700

    Update View Artikel isiartikel.php

--- application/views/artikel/isiartikel.php
+++ application/views/artikel/isiartikel.php
@@ -3,21 +3,17 @@
         <div class="row">
             <div class="col-lg-8 m-auto col-sm-12">
 
-                <h1 class=" section-judul judul-manfaat ">"Keutamaan membaca Al-Quran dengan baik dan Benar"</h1>
-                <p class="tanggalpost">Posted : 21 Januari 2020</p>
-                <p class="namaeditor">Mohammad Refaldo Rachman - Ldk Ishlah</p>
+                <h1 class=" section-judul judul-manfaat ">"<?= $detail->judul_artikel ?>"</h1>
+                <p class="tanggalpost">Posted : <?= date('d F Y', $detail->date_created); ?></p>
+                <p class="namaeditor"><?= $detail->post_by ?> - Ldk Ishlah</p>
 
             </div>
         </div>
         <div class="row">
             <div class="col-lg-8 m-auto col-sm-12">
-                <img src="<?= base_url('assets/img/12.png') ?>" alt="">
+                <img src="<?= base_url('assets/cover_artikel/' . $detail->cover_artikel) ?>" alt="">
                 <p class="isitulisan">
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eligendi reprehenderit pariatur porro laudantium voluptas iure rem, at architecto sit. Impedit, fugit. Cupiditate, sit! Perspiciatis consequuntur aspernatur a molestiae harum veniam iste fugit pariatur id. Minima nostrum libero assumenda rem!
-                    <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolore alias laborum aliquam earum vero dolorum harum natus, provident nesciunt mollitia ipsum error, eveniet, dolorem quibusdam ad asperiores corporis perspiciatis.
-                    <br>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere quo corrupti maiores eius blanditiis non quasi, explicabo molestiae labore, a excepturi ratione cumque nam error reprehenderit soluta, eaque sit eveniet fuga assumenda iste. Recusandae natus soluta ab quod repellendus, placeat maxime porro sint impedit provident aperiam fugiat dicta amet excepturi ipsum molestiae, earum animi perspiciatis ea, eos ipsa iusto at! Non provident rerum quasi nesciunt accusamus voluptatibus illum a temporibus animi aliquid accusantium blanditiis commodi, repellendus corrupti necessitatibus ipsa consequuntur ut eligendi recusandae. Quasi, obcaecati aliquam perferendis, corrupti fugiat ducimus mollitia numquam accusantium reiciendis velit harum eaque sint. Autem saepe maiores soluta veritatis, natus sed provident amet expedita iusto unde repellendus neque ducimus explicabo deserunt sunt quidem nobis. Error quas illum accusantium laudantium quaerat nulla, molestias perferendis eveniet commodi porro voluptates voluptate et illo quis quidem itaque aspernatur ipsum suscipit quae repellendus, atque est rerum distinctio. Officia rem voluptatibus est, inventore officiis veritatis illo aliquid molestiae accusamus nesciunt pariatur rerum aut soluta debitis veniam consectetur nostrum magnam tenetur optio qui eveniet ad. Excepturi obcaecati asperiores est voluptas magnam. Fugit totam consequuntur qui quam. Vero labore in nulla facilis delectus ipsum deserunt provident rerum quas distinctio maiores reiciendis quos tempora cum harum, excepturi enim laudantium qui aspernatur accusamus accusantium? Quia illo ut tempora ratione esse nihil repellendus voluptatum explicabo, soluta rerum beatae nam in consequuntur quas qui, accusamus ipsam neque laboriosam. Aperiam doloremque laudantium quibusdam vel. Velit quos ratione facere commodi quam animi nihil eligendi fuga labore ipsa rerum dolorum dolorem consequatur deleniti corporis, temporibus cupiditate ut architecto explicabo doloribus porro recusandae. Rerum eius minus hic quam quos dolores inventore laborum quia illo autem, corrupti asperiores fuga, vitae perspiciatis ullam officia recusandae sapiente adipisci tenetur atque quo doloribus placeat voluptatum. Blanditiis commodi officia vitae at, dicta voluptatum dolore ratione, nostrum temporibus voluptates corrupti itaque iusto sapiente dolorem animi dignissimos delectus adipisci nihil impedit corporis eligendi nobis aliquam sit. Reiciendis adipisci accusantium excepturi perferendis ipsum magni quam dicta. Esse quas, commodi quia, debitis recusandae corporis voluptates repellendus neque dicta laudantium atque unde cumque iste ducimus officia perspiciatis eius architecto quasi quo, delectus id assumenda ipsam? Quam accusantium aspernatur sit commodi incidunt sed accusamus delectus suscipit in corrupti! Rerum odit quaerat possimus deleniti vitae ipsa quo alias obcaecati laboriosam, eaque hic vel minima, debitis atque cupiditate nemo maiores. Corrupti quisquam quo non in! Blanditiis accusamus iusto repudiandae quae a rerum exercitationem aperiam nihil!
+                    <?= $detail->isi_artikel ?>
                 </p>
             </div>
 
@@ -130,44 +126,24 @@
                 </div>
             </div>
             <div class="row ">
+                <?php
+                foreach ($new_artikel as $data) {
+                ?>
+                    <div class="col-12 col-lg-4  col-md-4 col-sm-12 content-1">
+                        <figure class="">
+                            <a href="<?= base_url('artikel/isiartikel/' . $data->id) ?>" class="text-decoration-none">
+                                <img src="<?= base_url('assets/cover_artikel/' . $data->cover_artikel) ?>" class="img-fluid rounded gambarartikel" alt="...">
 
-                <div class="col-12 col-lg-4  col-md-4 col-sm-12 content-1">
-                    <figure class="">
-                        <a href="" class="text-decoration-none">
-                            <img src="<?= base_url('assets/img/12.png') ?>" class="img-fluid rounded gambarartikel" alt="...">
+                                <figcaption class="figure-caption capt">
+                                    <p>Posted : <?= date('d F Y', $data->date_created); ?></p>
 
-                            <figcaption class="figure-caption capt">
-                                <p>Posted : 21 Januari 2020</p>
+                                    <h6><?= $data->judul_artikel ?></h6>
+                                </figcaption>
+                            </a>
+                        </figure>
+                    </div>
+                <?php } ?>
 
-                                <h6>Keutamaan membaca Al-Quran dengan baik dan Benar </h6>
-                            </figcaption>
-                        </a>
-                    </figure>
-                </div>
-                <div class="col-12 col-lg-4 col-md-4 col-sm-12 content-2">
-                    <figure class="">
-                        <a href="" class="text-decoration-none">
-                            <img src="<?= base_url('assets/img/gambar2.png') ?>" class="img-fluid  rounded gambarartikel" alt="...">
-
-                            <figcaption class="figure-caption capt">
-                                <p>Posted : 21 Januari 2020</p>
-                                <h6>Keutamaan membaca Al-Quran dengan baik dan Benar </h6>
-                            </figcaption>
-                        </a>
-                    </figure>
-                </div>
-                <div class="col-12 col-lg-4 col-md-4 col-sm-12 content-3">
-                    <figure class="">
-                        <a href="" class="text-decoration-none">
-                            <img src="<?= base_url('assets/img/gambar1.png') ?>" class="img-fluid  rounded gambarartikel" alt="...">
-
-                            <figcaption class="figure-caption capt">
-                                <p>Posted : 21 Januari 2020</p>
-                                <h6>Keutamaan membaca Al-Quran dengan baik dan Benar </h6>
-                            </figcaption>
-                        </a>
-                    </figure>
-                </div>
             </div>
             <div class="row text-center buttonl ">
                 <div class="col-12 ">
