ccf935ba7c121e9a785ec16bba7171ca3c4e2261
refaldodev <m.refaldorachman@gmail.com>
2021-03-18 16:50:10 +0700

    rapihkan code

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -3,18 +3,13 @@
 
      <!-- Main Content -->
      <div id="content">
-
-
-
          <!-- Begin Page Content -->
          <div class="container-fluid">
-
              <!-- Page Heading -->
              <div class="d-sm-flex align-items-center justify-content-between mb-4">
                  <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
                  <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
              </div>
-
              <!-- Content Row -->
              <div class="row">
 
@@ -101,9 +96,7 @@
 
 
              </div>
-
              <!-- Content Row -->
-
              <div class="row">
 
                  <!-- Area Chart -->
@@ -171,10 +164,7 @@
                  </div>
              </div>
 
-             
-
          </div>
-         <!-- /.container-fluid -->
 
      </div>
      <!-- End of Main Content -->
