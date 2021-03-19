7a27f8ab525fd5a0f4f319283af5b6102f9063a2
Winanda <nandamaksum4@gmail.com>
2021-03-19 16:43:17 +0700

    nambahin bantuan di footer

---
+++ application/views/home/bantuan.php
@@ -0,0 +1,19 @@
+<section class="tentangkami">
+    <div class="container ">
+        <div class="row">
+            <div class="col-12 tentangkami-container">
+                <h1 class="text-left  section-judul">Bantuan</h1>
+                <h3 class="text-justify">
+                  Apabila mendapatkan kendala dan ingin mendapatkan bantuan, silahkan hubungi nomor berikut :
+                  <br>
+                  <br>
+                  <i class="fas fa-phone-square-alt"></i>
+                  <span>087654543213</span></a>
+                  <span></span>
+                </h3>
+
+
+            </div>
+        </div>
+    </div>
+</section>
